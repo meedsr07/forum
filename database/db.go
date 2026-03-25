dbd02761e249073da581a871b53bdc0c8014a9bc
msarar <mohammedsarar7@gmail.com>
2026-03-25 12:23:16 +0100

    @msarar  database tables

--- database/db.go
+++ database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// linking go with database
+
+var DB *sql.DB
+
+func InitializeDB() {
+	var err error
+	// open a connection betwen go and database
+	DB, err = sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module forum
 
 go 1.24.5
+
+require github.com/mattn/go-sqlite3 v1.14.37 // indirect
