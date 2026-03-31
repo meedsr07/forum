9199c86f8b677b4f7c6dcc9979f48991975af79b
bouzerda0 <bouzerdaayman6@gmail.com>
2026-03-31 16:53:06 +0100

    handle register procces

--- handlers/register.go
+++ handlers/register.go
@@ -5,20 +5,30 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-func RegisterHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+var (
+	usernameRegex = regexp.MustCompile(`^[a-z]+_[a-z0-9]+$`)
+	emailRegex    = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+)
 
+var tmpl *template.Template
+
+// 2. The init() function runs ONLY ONCE when the server starts
+func init() {
+	// Parse all HTML files in the "templates" folder and save them in "tmpl"
+	// template.Must() will crash the server immediately if there is a typo in HTML files
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+func RegisterHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// 1. If GET request: Show the register page
 	if r.Method == http.MethodGet {
-		tmpl, err := template.ParseFiles("templates/register.html")
-		if err != nil {
-			http.Error(w, "Error loading page", http.StatusInternalServerError)
-			return
-		}
-		tmpl.Execute(w, nil)
+		w.WriteHeader(http.StatusOK)
+		tmpl.ExecuteTemplate(w, "register.html", nil)
 		return
 	}
 
@@ -26,37 +36,95 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method == http.MethodPost {
 		err := r.ParseForm()
 		if err != nil {
-			http.Error(w, "Error reading form", http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Error reading form",
+			})
 			return
 		}
-
+		///////Email & Username check\\\\\\
 		username := r.FormValue("username")
 		email := r.FormValue("email")
-		password := r.FormValue("password")
 
-		if username == "" || email == "" || password == "" {
-			http.Error(w, "Please fill all fields", http.StatusBadRequest)
+		if len(username) < 4 || len(username) > 20 {
+			w.WriteHeader(http.StatusBadRequest)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Username length must be between 4 and 20 characters.",
+			})
 			return
 		}
 
+		if !usernameRegex.MatchString(username) {
+			w.WriteHeader(http.StatusBadRequest)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Username must be 3-20 characters long and contain only letters, numbers, and underscores.",
+			})
+			return
+		}
+
+		if !emailRegex.MatchString(email) {
+			w.WriteHeader(http.StatusBadRequest)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Please enter a valid email address.",
+			})
+			return
+		}
 		var existingID int
 		err = db.QueryRow("SELECT id FROM users WHERE email = ? OR username = ?", email, username).Scan(&existingID)
 
 		if err != sql.ErrNoRows {
-			http.Error(w, "Email or Username already exists", http.StatusConflict)
+			w.WriteHeader(http.StatusConflict)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Email or Username already exists",
+			})
+			return
+		}
+		///////Password check\\\\
+		password := r.FormValue("password")
+		// 3. Hash the password before saving it
+		if len(password) > 72 {
+			w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
+
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Password is too long! (72 characters max)",
+			})
+
+			return
+		}
+		if len(password) < 8 {
+			w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
+
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Password is too short! (8 characters minimum)",
+			})
+
+			return
+		}
+		if !strings.ContainsAny(password, "0123456789") {
+			w.WriteHeader(http.StatusBadRequest)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Password must contain at least one number",
+			})
 			return
 		}
 
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
-			http.Error(w, "Server error", http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Bcrypt hash error:", err)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Error hashing password",
+			})
 			return
 		}
 
 		_, err = db.Exec("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", username, email, string(hashedPassword))
 		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
 			log.Println("Database insert error:", err)
-			http.Error(w, "Could not create account", http.StatusInternalServerError)
+			tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+				"Error": "Could not create account",
+			})
 			return
 		}
 
@@ -66,5 +134,8 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	}
 
 	// 3. Reject anything else
-	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	tmpl.ExecuteTemplate(w, "register.html", map[string]interface{}{
+		"Error": "Method Not Allowed",
+	})
 }
