1b99c347118f635dfde7d598adbd06d56950d73d
bouzerda0 <bouzerdaayman6@gmail.com>
2026-03-31 21:32:52 +0100

    add session table in database

--- database/schema.sql
+++ database/schema.sql
@@ -57,3 +57,11 @@ CREATE TABLE IF NOT EXISTS likes (
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (comment_id) REFERENCES comments(id)
 );
+       ------session table-------
+CREATE TABLE IF NOT EXISTS sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    session_token TEXT UNIQUE NOT NULL,
+    expires_at DATETIME NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
