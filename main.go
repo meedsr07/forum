a5aa1a390ace4746d4dda123771009d763aedcea
msarar <mohammedsarar7@gmail.com>
2026-04-01 15:19:20 +0100

    database

--- main.go
+++ main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	"forum/database"
-	"forum/handlers"
 )
 
 func main() {
-	database.InitializeDB()
-	// Allpost, err := database.Getallpost(database.DB)
-	// if err != nil {
-	// 	fmt.Println("error in reading database")
-	// 	return
-	// }
-	// fmt.Println(Allpost)
-	// UserPosts , err := database.GetMyPosts(database.DB , 1)
-	// if err  !=  nil {
-	// 	fmt.Println("error in git user posts")
-	// 	return
-	// }
-	// fmt.Println(UserPosts)
-	// likedPost , err :=  database.GetLikedPosts(database.DB , 1)
-	// if err != nil {
-	// 	fmt.Println("error in geting liked postes")
-	// 	return
-	// }
-	// fmt.Println(likedPost)
-	usersesion , err := handlers.GetUserSession(database.DB)
+	DB, err := sql.Open("sqlite3", "./forum.db")
 	if err != nil {
 		return
 	}
-	fmt.Println(usersesion)
+	err1 := database.SeedDB(DB)
+	if err1 != nil {
+		fmt.Println("a")
+		return
+	}
 }
